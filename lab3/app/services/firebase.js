d9a8238639fd86e0516cc126f1755f40701b8253
Bartosz <96384251+hisoroo@users.noreply.github.com>
2025-05-04 23:36:18 +0200

    feat: update Firebase configuration to use environment variable for API key

--- lab3/app/services/firebase.js
+++ lab3/app/services/firebase.js
@@ -1,6 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,17 +7,17 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnMti5KFJkKBVmJqaTzX4G4cySwFkOhbM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "piwo-f7c12.firebaseapp.com",
   projectId: "piwo-f7c12",
   storageBucket: "piwo-f7c12.firebasestorage.app",
   messagingSenderId: "771556702303",
   appId: "1:771556702303:web:a5a2efa1b6248c479b83ea",
-  measurementId: "G-S8NPQEVXSY"
+  measurementId: "G-S8NPQEVXSY",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
