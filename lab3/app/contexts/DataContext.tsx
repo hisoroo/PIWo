112ca26e9d6ce37341598cf742b5726ebbfc3ff5
Bartosz <96384251+hisoroo@users.noreply.github.com>
2025-05-04 23:39:36 +0200

    feat: implement book fetching from database in BookProvider

--- lab3/app/contexts/DataContext.tsx
+++ lab3/app/contexts/DataContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from "react";
-import { Book, Cover } from "../interfaces/Book.tsx";
+import React, { createContext, useState, useEffect } from "react";
+import { Book } from "../interfaces/Book.tsx";
+import { readBooks } from "../services/BookService";
 
 export interface BookContextType {
   bookList: Book[];
@@ -13,52 +14,16 @@ const defaultContextValue: BookContextType = {
 export const BookContext = createContext<BookContextType>(defaultContextValue);
 
 export const BookProvider = ({ children }) => {
-  const [bookList, setBookList] = useState<Book[]>([
-    {
-      id: 1,
-      img_path: "https://picsum.photos/200/300",
-      title: "Obcy",
-      author: "Albert Camus",
-      genre: "Filozoficzna",
-      price: 19.99,
-      num_pages: 123,
-      cover: Cover.Paperback,
-      description: "Egzystencjalna powieść o absurdzie życia i alienacji.",
-    },
-    {
-      id: 2,
-      img_path: "https://picsum.photos/200/300",
-      title: "Rok 1984",
-      author: "George Orwell",
-      genre: "Dystopia",
-      price: 24.99,
-      num_pages: 328,
-      cover: Cover.Hardcover,
-      description: "Dystopijna wizja totalitarnego społeczeństwa.",
-    },
-    {
-      id: 3,
-      img_path: "https://picsum.photos/200/300",
-      title: "Duma i uprzedzenie",
-      author: "Jane Austen",
-      genre: "Romans",
-      price: 14.99,
-      num_pages: 279,
-      cover: Cover.Paperback,
-      description: "Klasyczna powieść romantyczna o miłości i społeczeństwie.",
-    },
-    {
-      id: 4,
-      img_path: "https://picsum.photos/200/300",
-      title: "Zbrodnia i kara",
-      author: "Fiodor Dostojewski",
-      genre: "Psychologiczna",
-      price: 29.99,
-      num_pages: 671,
-      cover: Cover.Hardcover,
-      description: "Psychologiczna powieść o winie, karze i moralności.",
-    },
-  ]);
+  const [bookList, setBookList] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const booksFromDb = await readBooks();
+      setBookList(booksFromDb);
+    };
+
+    fetchBooks();
+  }, []);
 
   return (
     <BookContext.Provider value={{ bookList, setBookList }}>
